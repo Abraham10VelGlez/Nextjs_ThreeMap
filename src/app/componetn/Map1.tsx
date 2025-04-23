f70f75e75d8d41793558654e20fa467348a297c7
Abraham10VelGlez <usbrifa@gmail.com>
2025-04-23 10:44:13 -0600

    refresh_map_con_cube_estables1

--- src/app/componetn/Map1.tsx
+++ src/app/componetn/Map1.tsx
@@ -1,17 +1,18 @@
 "use client"
-
 import * as React from 'react';
 import Map from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import '../css/styles.css'
-import { Canvas, useFrame, Vector3 } from '@react-three/fiber';
+// import { Canvas, useFrame, Vector3 } from '@react-three/fiber'; /// TRABAJA BIEN SIN EMBARGO LOS MODELOS 3D SALEN SOBRE EL MAPA Y NO SE QUEDAN DE FORMA ESTATICA O GEOREFERENCIADA
+import { useFrame, Vector3 } from '@react-three/fiber';
+import { Canvas } from "react-three-map/maplibre";/// PERMITE A LOS MODELOS 3D ESTAR DE FORMA ESTATICA O GEOREFERENCIADA
 import { Leva } from 'leva';
 import { Mesh } from "three";
 
 export function Map1() {
     return <>
         <Leva theme={{ sizes: { rootWidth: '340px', controlWidth: '150px' } }} />
-        <div id='html_'>
+        <div id='html_' >
             <Map
                 canvasContextAttributes={{
                     antialias: true,
@@ -23,17 +24,23 @@ export function Map1() {
                     pitch: 70,
                 }}
                 style={{ width: 1000, height: 1000 }}
-                // mapStyle="https://api.maptiler.com/maps/streets/style.json?key=TStVyyIZui04u4D9gGHQ"
                 mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
             >
-                <Canvas style={{ pointerEvents: 'none' }} latitude={51} longitude={0}>
+                <Canvas
+                    // style={{
+                    //     position: 'absolute',
+                    //     top: 0,
+                    //     left: 0,
+                    //     width: '100%',
+                    //     height: '100%',
+                    //     pointerEvents: 'none',
+                    // }}
+                    // style={{ pointerEvents: 'none' }}
+                    latitude={19.292536964352422} longitude={-99.65691804395397}>
                     <hemisphereLight args={["#ffffff", "#60666C"]} position={[1, 4.5, 3]} intensity={Math.PI} />
-                    <object3D>
-                        {/* <mesh>
-                            <boxGeometry args={[1, 1, 1]} />
-                            <meshStandardMaterial />
-                        </mesh> */}
-                        <Cube position={[0, 0, 0]}></Cube>
+
+                    <object3D scale={100}>
+                        <Cube position={[15, 0, 0]} />
                     </object3D>
                 </Canvas>
             </Map>
@@ -41,13 +48,10 @@ export function Map1() {
     </>
 }
 
-
 const Cube: React.FC<{ position: Vector3 }> = () => {
     const ref = React.useRef<Mesh>(null)
-    // Hold state for hovered and clicked events
     const [hovered, hover] = React.useState(false)
     const [clicked, click] = React.useState(false)
-    // Subscribe this component to the render-loop, rotate the mesh every frame
     useFrame((_state, delta) => {
         if (!ref.current) return;
         ref.current.rotation.x += delta;
